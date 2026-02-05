fe1269e63c59d0fffaa82aa0a4d35d02baf4c997
Pranav Yogesh <Yogeshpranav218@gmail.com>
2026-02-05 21:52:31 +0530

    Add .gitignore and remove build artifacts

--- build/ros2_service_server/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pranavyogesh/ros2_ws/assignment-ros2-c-service-server-PranavYogesh4
-# Build directory: /home/pranavyogesh/ros2_ws/assignment-ros2-c-service-server-PranavYogesh4/build/ros2_service_server
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
